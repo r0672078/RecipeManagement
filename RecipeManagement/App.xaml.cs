625e22c74e561a6b4ea79368e11a1c8ca4534b1d
Stef Simons <r0672078@student.thomasmore.be>
2023-08-09 12:42:36 +0200

    Initial commit

---
+++ RecipeManagement/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using RecipeManagement.Viewmodels;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RecipeManagement
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainViewModel viewModel = new MainViewModel();
+            Views.MainView view = new Views.MainView();
+            view.DataContext = viewModel;
+            view.Show();
+        }
+
+    }
+}
