27610ce361fddb980d27f891504e308cc5534d59
Stef Simons <r0672078@student.thomasmore.be>
2023-08-10 20:25:26 +0200

    Added IRepository and Repository

---
+++ RecipeManagement/Data/Repositories/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeManagement.Data.Repositories
+{
+    public interface IRepository<T>
+        where T : class, new()
+    {
+        IEnumerable<T> Retrieve();
+        int Add(T entity);
+        int Update(T entity);
+        int Delete(T entity);
+        IEnumerable<T> Retrieve(Expression<Func<T, bool>> conditions);
+        IEnumerable<T> Retrieve(params Expression<Func<T, object>>[] includes);
+        IEnumerable<T> Retrieve(Expression<Func<T, bool>> conditions,
+            params Expression<Func<T, object>>[] includes);
+        T SearchOnPK<TPrimaryKey>(TPrimaryKey id);
+        int AddOrUpdate(T entity);
+        int TAddRange(IEnumerable<T> entities);
+        int Delete<TPrimaryKey>(TPrimaryKey id);
+        int DeleteRange(IEnumerable<T> entities);
+    }
+}
---
+++ RecipeManagement/Data/Repositories/Repository.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace RecipeManagement.Data.Repositories
+{
+    public class Repository<T> : IRepository<T>
+        where T : class, new()
+    {
+        protected DbContext Context { get; }
+
+        public Repository(DbContext context)
+        {
+            this.Context = context;
+        }
+
+        public IEnumerable<T> Retrieve()
+        {
+            return Context.Set<T>().ToList();
+        }
+
+        public int Add(T entity)
+        {
+            Context.Entry(entity).State = EntityState.Added;
+            return Context.SaveChanges();
+        }
+
+        public int Update(T entity)
+        {
+            Context.Entry(entity).State = EntityState.Modified;
+            return Context.SaveChanges();
+        }
+
+        public int Delete(T entity)
+        {
+            Context.Entry(entity).State = EntityState.Deleted;
+            return Context.SaveChanges();
+        }
+
+        //uitbreiding
+
+        public IEnumerable<T> Retrieve(Expression<Func<T, bool>> conditions,
+           params Expression<Func<T, object>>[] includes)
+        {
+            IQueryable<T> query = Context.Set<T>();
+            if (includes != null)
+            {
+                foreach (var item in includes)
+                {
+                    query = query.Include(item);
+                }
+            }
+            if (conditions != null)
+            {
+                query = query.Where(conditions);
+            }
+            return query.ToList();
+        }
+
+        public IEnumerable<T> Retrieve(Expression<Func<T, bool>> conditions)
+        {
+            return Retrieve(conditions, null).ToList();
+        }
+
+        public IEnumerable<T> Retrieve(params Expression<Func<T, object>>[] includes)
+        {
+            return Retrieve(null, includes).ToList();
+        }
+
+        //handige functies
+        public T SearchOnPK<TPrimaryKey>(TPrimaryKey id)
+        {
+            return Context.Set<T>().Find(id);
+        }
+
+        public int AddOrUpdate(T entity)
+        {
+            // Wanneer de PK = 0, zal er worden toegevoegd
+            // Wanneer de PK > 0, zal er worden geupdatet
+            Context.Set<T>().Update(entity);
+            return Context.SaveChanges();
+        }
+
+        public int TAddRange(IEnumerable<T> entities)
+        {
+            Context.Set<T>().AddRange(entities);
+            return Context.SaveChanges();
+        }
+
+        public int Delete<TPrimaryKey>(TPrimaryKey id)
+        {
+            var entity = SearchOnPK(id);
+            Context.Set<T>().Remove(entity);
+            return Context.SaveChanges();
+        }
+
+        public int DeleteRange(IEnumerable<T> entities)
+        {
+            Context.Set<T>().RemoveRange(entities);
+            return Context.SaveChanges();
+        }
+    }
+}
